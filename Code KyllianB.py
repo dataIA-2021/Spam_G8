488e7ef497723ecfe3bce8768989068244e585d5
KyllianBD <93657252+KyllianBD@users.noreply.github.com>
2022-01-28 16:40:32 +0100

    Création d'une fonction classification sous Spyder

---
+++ Code KyllianB.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 13:53:59 2022
+
+@author: KyllianB
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+#%% Test types de séries
+
+df_test = pd.DataFrame({'Lettre':['A', 'B', 'C', 'D', 'E'], 
+                        'Voyelle':[True, False, False, False, True],
+                        'Itérations':[3, 1, 0, 2, 5], 
+                        'Ratio':[3/15 , 1/15 , 0/15 , 2/15 , 5/15]})
+
+#%%Test pour la fonction
+
+df = pd.read_csv('spam_clean.csv')
+Target = 'label'
+X = df.drop(columns = Target)
+
+print(X)
+
+#%%
+
+# data
+import pandas as pd
+# Preprocessing
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer, OneHotEncoder, StandardScaler, RobustScaler, MinMaxScaler
+# Pipeline and model
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+# Score of models
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+
+
+def classification(df, Target):
+    '''
+    
+
+    Parameters
+    ----------
+    df : Pandas.DataFrame
+        DataFrame sur lequel on souhaite entraîner et appliquer le modèle
+    
+    Target : str
+        Nom de la colonne contenant la  classification
+        
+    Random_seed : int, compris entre 0 et 999
+        Seed du random_state pour le train_test_split. The default is 42.
+    
+    Sortie :
+        Affiche le score du modèle et la matrice de confusion
+
+    '''
+    Target_Encoder = LabelEncoder()
+    y = Target_Encoder.fit_transform(df[Target])
+    
+    X = df.drop(columns = Target)
+    
+    column_cat = (df.select_dtypes(include = ['object']).drop(columns = Target)).columns
+    column_num = (df.select_dtypes(include = ['int', 'float'])).columns
+    
+    transformer_cat = Pipeline(steps = [
+        ('imputation', SimpleImputer(strategy='most_frequent')),
+        ('label encoder', LabelEncoder()), 
+        ('scaler', MinMaxScaler())])
+    
+    transformer_num = Pipeline(steps = [
+        ('imputation', SimpleImputer(strategy='median')),
+        ('scaling', MinMaxScaler())])
+    
+    preparation = ColumnTransformer(transformers =
+                                    [('data_num', transformer_num, column_num),
+                                     ('data_cat', transformer_cat, column_cat)])
+    
+    model = DecisionTreeClassifier()
+    
+    pipe_model = Pipeline(steps=
+                          [('preparation', preparation),
+                           ('model', model)])
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42, stratify = y)
+    
+    pipe_model.fit(X_train, y_train)
+    
+    y_pred = pipe_model.predict(X_test)
+    
+    print("Accuracy Score :", round(accuracy_score(y_test, y_pred), 5))
+    
+    ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred)).plot()
+    
+df = pd.read_csv('spam_features.csv')
+classification(df.drop(columns = 'content'), 'label')
+
+#%%
+
+#df.to_csv('spam_features.csv',index = False)
